56b8297944b774ae793a3c68a7c9fe67ceed6685
rafetefe <rafetefegazanfer@gmail.com>
2024-02-02 21:11:45 +0300

    folder initialization

---
+++ src/main/java/rafetefe/ecommerce/EcommercefunctionsApplication.java
@@ -0,0 +1,13 @@
+package rafetefe.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercefunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercefunctionsApplication.class, args);
+	}
+
+}
